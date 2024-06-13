256db3c8fe6f5cb1fc6a644369f7f3e7038932f2
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-06-13 10:13:27 -0400

    Gameplay Tags
    Created Gameplay tags, from data tables,  added gameplay tags to gameplay effects. Added gameplay effect delegates. Function to get all asset tags.

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -1,107 +1,107 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AbilitySystem/AuraAttributeSet.h"
 
+// Including necessary headers for ability system, networking, and character.
 #include "AbilitySystemBlueprintLibrary.h"
 #include "Net/UnrealNetwork.h"
 #include "GameFramework/Character.h"
 #include "GameplayEffectExtension.h"
 
-
+// Constructor implementation, initializing default values for attributes.
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	InitHealth(50.f);
-	InitMaxHealth(100.f);
-	InitMana(50.f);
-	InitMaxMana(100.f);
-	
-	
+    InitHealth(50.f);
+    InitMaxHealth(100.f);
+    InitMana(50.f);
+    InitMaxMana(100.f);
 }
 
+// Function to define which properties should be replicated.
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
-	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
 }
 
+// Called before an attribute's value is changed.
 void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
 {
-	Super::PreAttributeChange(Attribute, NewValue);
-	if (Attribute == GetHealthAttribute())
-	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
-	}
-	if (Attribute == GetManaAttribute())
-	{
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
-	}
+    Super::PreAttributeChange(Attribute, NewValue);
+    // Clamp Health and Mana to their respective maximum values.
+    if (Attribute == GetHealthAttribute())
+    {
+        NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHealth());
+    }
+    if (Attribute == GetManaAttribute())
+    {
+        NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+    }
 }
 
+// Helper function to set effect properties based on the callback data.
 void UAuraAttributeSet::SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const
 {
-	// Source = causer of the effect, Target = target of the effect (owner of this AS)
+    // Source = causer of the effect, Target = target of the effect (owner of this AS)
+    Props.EffectContextHandle = Data.EffectSpec.GetContext();
+    Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
 
-	Props.EffectContextHandle = Data.EffectSpec.GetContext();
-	Props.SourceASC = Props.EffectContextHandle.GetOriginalInstigatorAbilitySystemComponent();
+    if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
+    {
+        Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
+        Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
+        if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
+        {
+            if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
+            {
+                Props.SourceController = Pawn->GetController();
+            }
+        }
+        if (Props.SourceController)
+        {
+            Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
+        }
 
-	if (IsValid(Props.SourceASC) && Props.SourceASC->AbilityActorInfo.IsValid() && Props.SourceASC->AbilityActorInfo->AvatarActor.IsValid())
-	{
-		Props.SourceAvatarActor = Props.SourceASC->AbilityActorInfo->AvatarActor.Get();
-		Props.SourceController = Props.SourceASC->AbilityActorInfo->PlayerController.Get();
-		if (Props.SourceController == nullptr && Props.SourceAvatarActor != nullptr)
-		{
-			if (const APawn* Pawn = Cast<APawn>(Props.SourceAvatarActor))
-			{
-				Props.SourceController = Pawn->GetController();
-			}
-		}
-		if (Props.SourceController)
-		{
-			Props.SourceCharacter = Cast<ACharacter>(Props.SourceController->GetPawn());
-		}
-
-		if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
-		{
-			Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
-			Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
-			Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
-			Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
-			
-		}
-	}
+        if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+        {
+            Props.TargetAvatarActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+            Props.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+            Props.TargetCharacter = Cast<ACharacter>(Props.TargetAvatarActor);
+            Props.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Props.TargetAvatarActor);
+        }
+    }
 }
 
-
+// Called after a gameplay effect is executed.
 void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
 {
-	Super::PostGameplayEffectExecute(Data);
+    Super::PostGameplayEffectExecute(Data);
 
-	FEffectProperties Props;
-	SetEffectProperties(Data, Props);
-	
+    FEffectProperties Props;
+    SetEffectProperties(Data, Props);
 }
 
+// Notification functions called when attributes are replicated.
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
 }
 
 void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
 }
 
 void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
 }
 
 void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
 {
-	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
 }
-
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -1,95 +1,113 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
+// CoreMinimal.h is a common header file that includes the most frequently used headers in Unreal Engine.
 #include "CoreMinimal.h"
+// Includes for the AttributeSet, AbilitySystemComponent, and GameplayEffectExtension classes.
 #include "AttributeSet.h"
 #include "AbilitySystemComponent.h"
 #include "GameplayEffectExtension.h"
+// The generated.h file is required for Unreal's reflection system.
 #include "AuraAttributeSet.generated.h"
 
+// Macro to generate boilerplate code for getting, setting, and initializing attributes.
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
-	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
-	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
-	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
-	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+    GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+    GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+// A struct to store properties related to gameplay effects.
 USTRUCT()
 struct FEffectProperties
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
-	FEffectProperties() {}
-	FGameplayEffectContextHandle EffectContextHandle;
+    FEffectProperties() {}
+    
+    // Context handle for the gameplay effect.
+    FGameplayEffectContextHandle EffectContextHandle;
 
-	UPROPERTY()
-	UAbilitySystemComponent* SourceASC = nullptr;
+    // Source and target components and actors involved in the effect.
+    UPROPERTY()
+    UAbilitySystemComponent* SourceASC = nullptr;
 
-	UPROPERTY()
-	AActor* SourceAvatarActor = nullptr;
+    UPROPERTY()
+    AActor* SourceAvatarActor = nullptr;
 
-	UPROPERTY()
-	AController* SourceController = nullptr;
+    UPROPERTY()
+    AController* SourceController = nullptr;
 
-	UPROPERTY()
-	ACharacter* SourceCharacter = nullptr;
+    UPROPERTY()
+    ACharacter* SourceCharacter = nullptr;
 
-	UPROPERTY()
-	UAbilitySystemComponent* TargetASC = nullptr;
+    UPROPERTY()
+    UAbilitySystemComponent* TargetASC = nullptr;
 
-	UPROPERTY()
-	AActor* TargetAvatarActor = nullptr;
+    UPROPERTY()
+    AActor* TargetAvatarActor = nullptr;
 
-	UPROPERTY()
-	AController* TargetController = nullptr;
+    UPROPERTY()
+    AController* TargetController = nullptr;
 
-	UPROPERTY()
-	ACharacter* TargetCharacter = nullptr;
+    UPROPERTY()
+    ACharacter* TargetCharacter = nullptr;
 };
 
 /**
- * 
+ * UAuraAttributeSet class - defines attributes for the Aura game.
  */
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 public:
-	UAuraAttributeSet();
-	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+    // Constructor to initialize default values for the attributes.
+    UAuraAttributeSet();
+    
+    // Function to define which properties should be replicated.
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
-	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+    // Called before an attribute's value is changed.
+    virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+    
+    // Called after a gameplay effect is executed.
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
-	FGameplayAttributeData Health;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
+    // Health attribute, replicated with a notification function.
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+    FGameplayAttributeData Health;
+    // Generates boilerplate code for Health attribute.
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
 
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
-	FGameplayAttributeData MaxHealth;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
+    // MaxHealth attribute, replicated with a notification function.
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+    FGameplayAttributeData MaxHealth;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
 
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
-	FGameplayAttributeData Mana;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
-	
+    // Mana attribute, replicated with a notification function.
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+    FGameplayAttributeData Mana;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
 
-	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
-	FGameplayAttributeData MaxMana;
-	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
+    // MaxMana attribute, replicated with a notification function.
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+    FGameplayAttributeData MaxMana;
+    ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
 
-	UFUNCTION()
-	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+    // Notification functions called when attributes are replicated.
+    UFUNCTION()
+    void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 
-	UFUNCTION()
-	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+    UFUNCTION()
+    void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 
-	UFUNCTION()
-	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+    UFUNCTION()
+    void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
 
-	UFUNCTION()
-	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+    UFUNCTION()
+    void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 
 private:
-	void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
+    // Helper function to set effect properties based on the callback data.
+    void SetEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& Props) const;
 };
