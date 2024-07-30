628c7701dbaa2731e9402c478885a0c7374df183
Neil Lopes <127422402+PhArObiS@users.noreply.github.com>
2024-07-30 02:09:46 -0400

    Spell Menu
    Cost and Cooldown in Spell Description, Self Deselect, Equipped Spell Row Animations, Ability Tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -225,10 +225,18 @@ bool UAuraAbilitySystemComponent::GetDescriptionsByAbilityTag(const FGameplayTag
             OutDescription = AuraAbility->GetDescription(AbilitySpec->Level);
             OutNextLevelDescription = AuraAbility->GetNextLevelDescription(AbilitySpec->Level + 1);
             return true;
-        }
+        }  
     }
     const UAbilityInfo* AbilityInfo = UAuraAbilitySystemLibrary::GetAbilityInfo(GetAvatarActor());
-    OutDescription = UAuraGameplayAbility::GetLockedDescription(AbilityInfo->FindAbilityInfoForTag(AbilityTag).LevelRequirement);
+    if (!AbilityTag.IsValid() || AbilityTag.MatchesTagExact(FAuraGameplayTags::Get().Abilities_None))
+    {
+        OutDescription = FString();
+    }
+    else
+    {
+        OutDescription = UAuraGameplayAbility::GetLockedDescription(AbilityInfo->FindAbilityInfoForTag(AbilityTag).LevelRequirement);
+    }
+    
     OutNextLevelDescription = FString();
     return false;
 }
